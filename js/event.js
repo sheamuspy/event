c0aad13ed01a8687ffb9f3309881cab2639c5c37
sheamuspy <sheamuspy@gmail.com>
2015-11-29 13:30:42 +0000

    Basic filling of the event list added.

---
+++ js/event.js
@@ -0,0 +1,114 @@
+/*global $, jQuery, document, sendRequest, alert, sessionStorage, window, getEvents */
+
+
+$(document).ready(function () {
+    "use strict";
+
+    var userId = sessionStorage.getItem("user_id");
+    getEvents();
+
+});
+
+$(function () {
+    "use strict";
+    $("#addEventBtn").click(function () {
+        var eventOwner, eventName, eventDescription, eventDate, eventTime, eventVenue, URL, response;
+        eventOwner = sessionStorage.getItem("user_id");
+        eventName = $("#eventName").val();
+        eventDescription = $("#eventDescription").val();
+        eventDate = $("#eventDate").val();
+        eventTime = $("#eventTime").val();
+        eventVenue = $("#eventVenue").val();
+
+        URL = "http://localhost/mobile_web/Event/php/android_ajax.php?cmd=1&event_owner=" + eventOwner + "&event_name='" + eventName + "'&event_description='" + eventDescription + "'&event_date='" + eventDate + "'&event_time='" + eventTime + "'&event_venue=" + eventVenue;
+        response = sendRequest(URL);
+
+        if (response.status === 0) {
+            $("#eventName").val("");
+            $("#eventDescription").val("");
+            $("#eventDate").val("");
+            $("#eventTime").val("");
+            $("#eventVenue").val("");
+            alert("Added");
+        } else if (response.status === 2) {
+            alert("Please again try later");
+        } else {
+            alert("Please check your feilds.");
+        }
+    });
+});
+
+function getEvents() {
+    "use strict";
+    var userId, URL, URL2, response, response2, events, feedback, commented, i, j;
+
+    userId = '1';// sessionStorage.getItem("user_id");
+
+    URL = "http://localhost/mobile_web/Event/php/android_ajax.php?cmd=4&user_id=" + userId;
+    URL2 = "http://localhost/mobile_web/Event/php/android_ajax.php?cmd=8&user_id=" + userId;
+
+    response = sendRequest(URL);
+    response2 = sendRequest(URL2);
+    events = response.events;
+    feedback = response2.feedback;
+    j = 0;
+
+
+    for (i = 0; i < events.length; i = i + 1) {
+
+        commented = false;
+
+        $("#event-list").append("<div class='card-panel'>" +
+            "<ul>" +
+            "<li>" + events[i].EVENT_NAME + "</li>" +
+            "<li>" + events[i].EVENT_OWNER + "</li>" +
+            "<li>" + events[i].EVENT_DESCRIPTION + "</li>" +
+            "</ul>" +
+            "</div>");
+
+        for (j; j < feedback.length; j = j + 1) {
+            if (events[i].EVENT_ID !== feedback[j].SUBSCRIPTION_EVENT_ID) {
+                break;
+            } else if (feedback[j].SUBSCRIPTION_USER_ID === userId) {
+                commented = true;
+            }
+            $("#event-list").append("<div class='card-panel'>" +
+                "<td>" + feedback[j].FEEDBACK_COMMENT + "</td>" +
+                "<td>" + feedback[j].EVENT_OWNER + "</td>" +
+                "</tr>" +
+                "</div>");
+        }
+
+        if (commented === false) {
+            $("#event-list").append("<div class='card-panel'>" +
+                "<div class='input-field'>" +
+                    "<input placeholder='Comment' id='comment' type='text' class='validate'>" +
+                    "<label for='comment'>Comment</label>" +
+                "</div>" +
+                "<div class='input-field'>" +
+                    "<input placeholder='Rating' id='rating' type='text' class='validate'>" +
+                    "<label for='rating'>Rating</label>" +
+                "</div>" +
+                "<a class='waves-effect waves-light btn'>Stuff</a>" +
+                "</div>");
+        }
+    }
+
+}
+
+function logout() {
+    "use strict";
+    sessionStorage.clear();
+    window.location = "index.html";
+}
+
+function sendRequest(u) {
+    "use strict";
+    var obj, result;
+    obj = $.ajax({
+        url: u,
+        async: false
+    });
+    result = $.parseJSON(obj.responseText);
+    return result;
+}
