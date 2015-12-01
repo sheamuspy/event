a7f04caba79af373f9af92b1cc7d1db7b696967b
sheamuspy <sheamuspy@gmail.com>
2015-12-01 03:20:00 +0000

    Test1.

--- js/login.js
+++ js/login.js
@@ -14,33 +14,34 @@ function sendRequest(requestURL) {
 
 function login() {
     "use strict";
-    var username, password, url, response;
-
-    username = $("#loginUsername").val();
-    password = $("#loginPassword").val();
-
-    url = "http://cs.ashesi.edu.gh/~csashesi/class2016/sheamus-yebisi/mobile_web/Event/php/android_ajax.php?cmd=0&username='" + username + "'&password='" + password + "'";
-
-    response = sendRequest(url);
-
-    if (response.status === 0 && response.user != false) {
-        var userId = response.user['USER_ID'];
-        var username = response.user['USERNAME'];
-        var userFirstname = response.user['USER_FIRSTNAME'];
-        var userSurname = response.user['USER_SURNAME'];
-        var userEmail = response.user['USER_EMAIL'];
-        var userPicture = response.user['USER_PICTURE'];
-
-        sessionStorage.setItem("userId", userId);
-        sessionStorage.setItem("username", username);
-        sessionStorage.setItem("userFirstname", userFirstname);
-        sessionStorage.setItem("userSurname", userSurname);
-        sessionStorage.setItem("userEmail", userEmail);
-        sessionStorage.setItem("userPicture", userPicture);
-
-        window.location = "event.html";
-
-    } else {
-        Materialize.toast("Password or username is incorrect.", 2000);
-    }
+//    var username, password, url, response;
+//
+//    username = $("#loginUsername").val();
+//    password = $("#loginPassword").val();
+//
+//    url = "http://cs.ashesi.edu.gh/~csashesi/class2016/sheamus-yebisi/mobile_web/Event/php/android_ajax.php?cmd=0&username='" + username + "'&password='" + password + "'";
+//
+//    response = sendRequest(url);
+//
+//    if (response.status === 0 && response.user != false) {
+//        var userId = response.user['USER_ID'];
+//        var username = response.user['USERNAME'];
+//        var userFirstname = response.user['USER_FIRSTNAME'];
+//        var userSurname = response.user['USER_SURNAME'];
+//        var userEmail = response.user['USER_EMAIL'];
+//        var userPicture = response.user['USER_PICTURE'];
+//
+//        sessionStorage.setItem("userId", userId);
+//        sessionStorage.setItem("username", username);
+//        sessionStorage.setItem("userFirstname", userFirstname);
+//        sessionStorage.setItem("userSurname", userSurname);
+//        sessionStorage.setItem("userEmail", userEmail);
+//        sessionStorage.setItem("userPicture", userPicture);
+//
+//        window.location = "event.html";
+//
+//    } else {
+//        Materialize.toast("Password or username is incorrect.", 2000);
+//    }
+           Materialize.toast("Hi.", 2000);
 }
